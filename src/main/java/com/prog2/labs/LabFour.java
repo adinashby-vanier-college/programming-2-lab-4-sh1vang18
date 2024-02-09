b76be10f53d7aabb430f9ef3dd9c7e7ccdfc90a6
sh1vang18 <sh1vang@hotmail.com>
2024-02-08 20:15:06 -0500

    lab4

--- src/main/java/com/prog2/labs/LabFour.java
+++ src/main/java/com/prog2/labs/LabFour.java
@@ -1,4 +1,7 @@
 package com.prog2.labs;
+
+import java.util.Arrays;
+
 /**
  * @author adinashby
  *
@@ -18,11 +21,49 @@ public class LabFour {
 	 * Please refer to the README file for question(s) description
 	 */
 	
-    public int containerWithMostWater(int[] height) {
-        return -1;
+   public static int containerWithMostWater(int[] height) {
+
+        int i = 0;
+        int j = height.length - 1;
+        int ans = 0;
+        while(i < j){   
+            int temp = Math.min(height[i], height[j]) * (j - i) ;
+             ans = Math.max(ans, temp);
+
+             if(height[i] < height[j]){ 
+                i++;
+             }
+             else{  
+                j--;
+             }
+        }
+       
+        return ans;
     }
     
-    public int threeSumClosest(int[] nums, int target) {
-        return -1;
+    public static int threeSumClosest(int[] nums, int target) {
+
+        int ans = 0;
+        int temp = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length -2; i++){
+                int j = i + 1;
+                int k = nums.length -1;
+                while(j < k){   
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(Math.abs(sum-target) < temp ){   
+                        ans = sum;
+                        temp = Math.abs(sum-target);
+                    }
+                    if(sum > target){   
+                        k--;
+                    }
+                    else{   
+                        j++;
+                    }
+                }
+        }
+
+        return ans;
     }
 }
